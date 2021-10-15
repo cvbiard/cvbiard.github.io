02faffe232c266bdf686d8e461b15319a5790984
cvbiard <cvbiard@hotmail.com>
2021-10-14 18:13:54 -0700

    :D

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 
-    $("#ASIC").hover(function(){
-        $(this).css("color", "red");
-    });
+   
     const sect = document.querySelector('section');
     const para = document.createElement('p');
     para.textContent = 'This text is from a JS script, whoa';
+    para.id = "test";
     sect.appendChild(para);
     const text = document.createTextNode('-idk');
     const linkPara = document.querySelector('p');
-    linkPara.appendChile(text);
+    linkPara.appendChild(text);
+    $("#test").hover(function(){
+        $(this).css("color", "red");
+    });
 });
 
